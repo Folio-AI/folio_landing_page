32ad261a9ad824602be9c69ef5aea012db7d741f
William Das <williamhdas@gmail.com>
2023-10-10 05:19:07 -0400

    Pricing table alignments

--- components/pricing-tables.tsx
+++ components/pricing-tables.tsx
@@ -41,8 +41,8 @@ export default function PricingTables() {
                 >Bill Monthly</button>
               </div>
             </div> */}
-<div className="flex justify-center">
-            <div className="max-w-sm md:max-w-2xl xl:max-w-none mx-auto grid gap-8 md:grid-cols-2 xl:grid-cols-3 xl:gap-6 items-start">
+<div className="">
+            <div className="mx-auto grid gap-8 md:grid-cols-3 xl:grid-cols-3 xl:gap-6 items-start">
 
               {/* Pricing table 1 */}
               <div className="relative flex flex-col h-full py-5 px-6 rounded bg-white shadow-xl mx-auto text-center" data-aos="zoom-y-out" data-aos-delay="450">
@@ -139,8 +139,54 @@ export default function PricingTables() {
                 </div>
               </div>
 
+              <div className="relative flex flex-col h-full py-5 px-6 rounded bg-blue-100 shadow-xl border-2 border-blue-500 mx-auto text-center" data-aos="zoom-y-out" data-aos-delay="450">
+                <div className="absolute top-0 right-0 mr-5 p-3 -mt-5 bg-yellow-500 rounded-full">
+                  <svg className="w-4 h-4 fill-current text-white" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M15.145 5.05l-4.316-.627L8.898.513c-.338-.684-1.456-.684-1.794 0l-1.93 3.91-4.317.627a1.002 1.002 0 00-.554 1.707l3.124 3.044-.737 4.3a1 1 0 001.45 1.053L8 13.125l3.862 2.03c.728.381 1.59-.234 1.45-1.054l-.736-4.299L15.7 6.758a1.003 1.003 0 00-.555-1.708z" />
+                  </svg>
+                </div>
+                <div className="mb-4">
+                  <div className="text-lg font-bold mb-1">Professional</div>
+                  <div className="inline-flex items-baseline mb-2">
+                    <span className="text-3xl font-bold">$</span>
+                    <span className="text-4xl font-bold">{annual ? '19.99' : '34'}</span>
+                    <span className="text-gray-600 pl-2">/month</span>
+                  </div>
+                  <div className="text-lg text-gray-800"></div>
+                </div>
+                <ul className="text-gray-600 -mb-2 grow">
+                  <li className="flex items-center mb-2">
+                    <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Unlimited resumes per month.</span>
+                  </li>
+                  <li className="flex items-center mb-2">
+                    <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Unlimited cover letters per month.</span>
+                  </li>
+                  <li className="flex items-center mb-2">
+                    <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Advanced ATS Optimization.</span>
+                  </li>
+                  {/* <li className="flex items-center mb-2">
+                    <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Dedicated support</span>
+                  </li> */}
+                </ul>
+                <div className="border-t border-gray-200 pt-5 mt-6">
+                  <a className="btn-sm text-white bg-blue-600 hover:bg-blue-700 w-full" href="#0">Start free trial</a>
+                </div>
+              </div>
+
               {/* Pricing table 3 */}
-              <div className="relative flex flex-col h-full py-5 px-6 rounded bg-white shadow-xl" data-aos="zoom-y-out" data-aos-delay="450">
+              {/* <div className="relative flex flex-col h-full py-5 px-6 rounded bg-white shadow-xl text-center" data-aos="zoom-y-out" data-aos-delay="450">
                 <div className="mb-4">
                   <div className="text-lg font-bold mb-1">Professional</div>
                   <div className="inline-flex items-baseline mb-2">
@@ -168,18 +214,18 @@ export default function PricingTables() {
                       <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                     </svg>
                     <span>Advanced ATS Optimization.</span>
-                  </li>
+                  </li> */}
                   {/* <li className="flex items-center mb-2">
                     <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                       <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                     </svg>
                     <span>Dedicated support</span>
                   </li> */}
-                </ul>
+                {/* </ul>
                 <div className="border-t border-gray-200 pt-5 mt-6">
                   <a className="btn-sm text-white bg-blue-600 hover:bg-blue-700 w-full" href="#0">Start free trial</a>
                 </div>
-              </div>
+              </div> */}
 
               {/* Pricing table 4 */}
               {/* <div className="relative flex flex-col h-full py-5 px-6 rounded bg-white shadow-xl" data-aos="zoom-y-out" data-aos-delay="450">
