a68a6eba79426cb3cbad88eba927e682102aa1ad
William Das <williamhdas@gmail.com>
2023-10-13 03:52:04 -0400

    Resume UI changes

--- components/postprocess.jsx
+++ components/postprocess.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect } from 'react';
 import ResumeEditor from './resume-editor';
 import ResumePDF from './pdf';
 import { Document, Page } from 'react-pdf';
@@ -10,7 +10,7 @@ import "react-pdf/dist/esm/Page/TextLayer.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-function ProcessBox() {
+export default function Postprocess() {
   const [sharedState, setSharedState] = useState(exampleData);
   const [editor, setEditor] = useState(<ResumeEditor resumeData={exampleData} updateSharedState={setSharedState}/>)
   const [linkToPDFBlob, setLinkToPDFBlob] = useState(null);
@@ -19,19 +19,36 @@ function ProcessBox() {
   const [currPage, setCurrPage] = useState(null);
   const [delayedSharedState, setDelayedSharedState] = useState(sharedState);
   
-  // useEffect(() => {
-  //   // Introduce delay mechanism here
-  //   const delayUpdate = setTimeout(() => {
-  //     setDelayedSharedState(sharedState);
-  //   }, 750);  // 1 second delay
+  useEffect(() => {
+    // Introduce delay mechanism here
+    const delayUpdate = setTimeout(() => {
+      setDelayedSharedState(sharedState);
+    }, 750);  // 1 second delay
 
-  //   return () => clearTimeout(delayUpdate);  // Clean up on component unmount or if sharedState changes before delay elapses
-  // }, [sharedState]);
+    return () => clearTimeout(delayUpdate);  // Clean up on component unmount or if sharedState changes before delay elapses
+  }, [sharedState]);
 
   function handleRefresh() {
     setDelayedSharedState(sharedState);
   }
 
+  function handleDownloadAsWord() {
+    // Implement the download as Word logic here
+    console.log('Download as Word clicked');
+  }
+
+  function handleDownloadAsPDF() {
+    // Implement the download as PDF logic here
+    console.log('Download as PDF clicked');
+  }
+
+  function derenderDownload() {
+    setDocLoading(true);
+  }
+
+  function renderDownload() {
+    setDocLoading(false);
+  }
 
   useEffect(() => {
     async function renderPDF() {
@@ -41,7 +58,7 @@ function ProcessBox() {
       const urlToPDFBlob = URL.createObjectURL(pdfBlob);
       setLinkToPDFBlob(urlToPDFBlob);
       const page = <Page loading={currPage} pageNumber={1} width={500} renderAnnotationLayer={false} />;
-      const doc = <Document file={urlToPDFBlob} loading={document}>{page}</Document>
+      const doc = <Document file={urlToPDFBlob} loading={derenderDownload} onLoadProgress={derenderDownload} onLoadSuccess={renderDownload}>{page}</Document>
       setDocument(doc);
       setCurrPage(page);
     }
@@ -61,27 +78,26 @@ function ProcessBox() {
     }
     updatePDF();
   }, [delayedSharedState]);
-  
 
 
   return (
-    <div className="flex">
-      <div className="border border-gray-300 rounded w-1/2 pr-4">
-        <button className="bg-green-400 hover:bg-teal-500 text-white font-bold py-2 px-4 rounded mb-4 p-4" onClick={handleRefresh}>
-          Refresh
-        </button>
-        <div className="p-4 scrollable-div" style={{height: 'calc(100vh - 40px)', overflow: 'auto'}}>
-          {editor}
-        </div>
-      </div>
-      <div className="w-1/2 pl-4 border border-gray-300 rounded p-4">
-        {document}
-      </div>
-    </div>
-  );
-  
-}
+    <div className="grid grid-cols-2 gap-4">
+      
 
-export default function Postprocess() {
-  return ProcessBox();
+    <div className="border border-gray-300 rounded pr-4 col-span-1 shadow-xl">
+      {/* <button className="bg-green-400 hover:bg-teal-500 text-white font-bold py-2 px-4 rounded" onClick={handleRefresh}>
+              Refresh
+      </button> */}
+
+        <div className="text-sm">
+            {editor}
+        </div>
+    </div>
+
+    <div className="pl-4 border border-gray-300 rounded p-4 fixed col-span-1 right-10 shadow-2xl">
+        {document}
+    </div>
+</div>
+
+  );
 }
